921ae6af0961a57ae8ecb71d022843857e84d58c
notkaswan <deepak.kaswan.1999@gmail.com>
2022-12-15 19:17:16 +0530

    modified:   src/App.css 	modified:   src/components/Meme.js 	new file:   src/memesData.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,19 @@
 import React from "react"
+import memeData from "../memesData"
+
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text" 
                     placeholder="Top text" 
@@ -16,10 +26,12 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={memeImage} className="meme--image" alt="" />
         </main>
     )
 }

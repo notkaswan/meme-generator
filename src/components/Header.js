c8dcc2e4ceb5202e422f48bdad3b07bcdefac569
notkaswan <deepak.kaswan.1999@gmail.com>
2022-12-15 14:18:49 +0530

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img className="header--image" src="./images/trollface.png" alt="" />
-            <h1 className="header--title">Header Component</h1>
+            <h1 className="header--title">Meme Generator</h1>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )

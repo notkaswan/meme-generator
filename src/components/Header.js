a98b943c02d84e133e290e3a99c50564e3fd514b
notkaswan <deepak.kaswan.1999@gmail.com>
2022-12-15 13:28:52 +0530

    deleted:    public/favicon.ico 	new file:   public/images/trollface.png 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Header.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="./images/trollface.png" alt="" />
+            <h1>Header Component</h1>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
